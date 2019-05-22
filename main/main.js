6305fd73877e8466384bcc13fe4b026615424a3a
Kang Liu <sc17kl@leeds.ac.uk>
2019-05-22 15:48:11 +0800

    Finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    var a=[];
+    var head=['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+    var mid=['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+    var tail=['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+    var s='';
+
+    a.push(head[str[0]-'0']);
+    a.push(mid[str[0]-'0']);
+    a.push(tail[str[0]-'0']);
+    for(var i=1;i<str.length;i++)
+    {
+        a[0]+=' ';
+        a[1]+=' ';
+        a[2]+=' ';
+        a[0]+=head[str[i]-'0'];
+        a[1]+=mid[str[i]-'0'];
+        a[2]+=tail[str[i]-'0'];
+    }
+    for(var i=0;i<3;i++)
+    {
+        s+=a[i];
+        s+='\n';
+    }
+    return s;
 };
